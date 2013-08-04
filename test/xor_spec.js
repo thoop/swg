a98ea605250a6d485acfcd4fffb467dfbb6f019b
Todd Hooper <thooper@rallydev.com>
2013-08-04 00:17:51 -0600

    added correct defaults and way to set them

--- test/xor_spec.js
+++ test/xor_spec.js
@@ -21,7 +21,8 @@ describe('xor', function() {
             var expected = 'dd96edebe5a08038cfbb863ec9bea628ccdc18435f334539e08c2ad87ac7c5a43830dc60d7633abd6559d8da373fbd203cbe01eb510dac57f587baa91f184cd6e1cae8feac8beed6cceec61f0421e91f0418d900633435';
 
             var encrypted = xor.encrypt({
-                buffer: new Buffer(unencrypted, 'hex')
+                buffer: new Buffer(unencrypted, 'hex'),
+                crcSeed: 'a50a8e8a'
             });
             encrypted.toString('hex').should.equal(expected);
         })
@@ -45,7 +46,8 @@ describe('xor', function() {
             var expected = '789c636138366dd32a1b0606060520160562be6b93ef5d7abfbf6fe19a4bef7c42f719c4ef53e6ddb23ae267526665fa0b81bccb6db3adbca48a16feea9ff67ffed2a4284d410628606528c9c8cf2f000039301f673031';
 
             var decrypted = xor.decrypt({
-                buffer: new Buffer(encrypted, 'hex')
+                buffer: new Buffer(encrypted, 'hex'),
+                crcSeed: 'a50a8e8a'
             });
             decrypted.toString('hex').should.equal(expected);
 
