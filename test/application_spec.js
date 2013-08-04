f9b6626f41f7610ac38094767fa32ead9fd1b883
Todd Hooper <thooper@rallydev.com>
2013-08-04 00:27:47 -0600

    changed application to be more prototype-like than object-like

--- test/application_spec.js
+++ test/application_spec.js
@@ -1,5 +1,5 @@
 var should = require('should')
-var application = require('../lib/application')
+var Application = require('../lib/application')
 
 describe('application', function() {
 
@@ -9,7 +9,7 @@ describe('application', function() {
             var example_unencrypted = '000900010002AB43E3D500FF001145327643D4F100'; //took off ABCD crc value since CRC comes after encryption
             var example_encrypted = '00091D4F3285B60CD150B6F3D141F3C1A7022702F3'; //took off ABCD crc value since CRC comes after encryption
 
-            var encrypted = application.encrypt(new Buffer(example_unencrypted, 'hex'), '1D4E3287');
+            var encrypted = Application.prototype.encrypt(new Buffer(example_unencrypted, 'hex'), '1D4E3287');
             encrypted.toString('hex').toUpperCase().should.equal(example_encrypted);
         })
     })
@@ -20,7 +20,7 @@ describe('application', function() {
             var example_unencrypted = '000900010002AB43E3D500FF001145327643D4F100';
             var example_encrypted = '00091D4F3285B60CD150B6F3D141F3C1A7022702F3';
 
-            var encrypted = application.decrypt(new Buffer(example_encrypted, 'hex'), '1D4E3287');
+            var encrypted = Application.prototype.decrypt(new Buffer(example_encrypted, 'hex'), '1D4E3287');
             encrypted.toString('hex').toUpperCase().should.equal(example_unencrypted);
         })
     })
@@ -29,7 +29,7 @@ describe('application', function() {
 
         it('should throw an error if the crc is invalid', function() {
             try {
-                application.checkAndRemoveCRC(new Buffer('00091D4F3285B60CD150B6F3D141F3C1A7022702F3', 'hex'));
+                Application.prototype.checkAndRemoveCRC(new Buffer('00091D4F3285B60CD150B6F3D141F3C1A7022702F3', 'hex'));
             } catch (err) {
                 return;
             }
@@ -38,7 +38,7 @@ describe('application', function() {
 
         it('should check and remove the CRC from the buffer if the CRC is valid', function() {
 
-            var newBuffer = application.checkAndRemoveCRC(new Buffer('00091D4F3285B60CD150B6F3D141F3C1A702276dE4', 'hex'));
+            var newBuffer = Application.prototype.checkAndRemoveCRC(new Buffer('00091D4F3285B60CD150B6F3D141F3C1A702276dE4', 'hex'));
             newBuffer.toString('hex').toUpperCase().should.equal('00091D4F3285B60CD150B6F3D141F3C1A70227');
         })
     })
