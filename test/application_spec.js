5a77e55c4e456788430e0b34215316121bf2467c
Todd Hooper <todd@prerender.io>
2016-09-19 23:05:41 -0500

    beautify and update dependencies

--- test/application_spec.js
+++ test/application_spec.js
@@ -3,44 +3,44 @@ var Application = require('../lib/application')
 
 describe('application', function() {
 
-    describe('encrypt', function() {
+	describe('encrypt', function() {
 
-        it('should encrypt the anh example correctly', function() {
-            var example_unencrypted = '000900010002AB43E3D500FF001145327643D4F100'; //took off ABCD crc value since CRC comes after encryption
-            var example_encrypted = '00091D4F3285B60CD150B6F3D141F3C1A7022702F3'; //took off ABCD crc value since CRC comes after encryption
+		it('should encrypt the anh example correctly', function() {
+			var example_unencrypted = '000900010002AB43E3D500FF001145327643D4F100'; //took off ABCD crc value since CRC comes after encryption
+			var example_encrypted = '00091D4F3285B60CD150B6F3D141F3C1A7022702F3'; //took off ABCD crc value since CRC comes after encryption
 
-            var encrypted = Application.prototype.encrypt(new Buffer(example_unencrypted, 'hex'), '1D4E3287');
-            encrypted.toString('hex').toUpperCase().should.equal(example_encrypted);
-        })
-    })
+			var encrypted = Application.prototype.encrypt(new Buffer(example_unencrypted, 'hex'), '1D4E3287');
+			encrypted.toString('hex').toUpperCase().should.equal(example_encrypted);
+		})
+	})
 
-    describe('decrypt', function() {
+	describe('decrypt', function() {
 
-        it('should decrypt the anh example correctly', function() {
-            var example_unencrypted = '000900010002AB43E3D500FF001145327643D4F100';
-            var example_encrypted = '00091D4F3285B60CD150B6F3D141F3C1A7022702F3';
+		it('should decrypt the anh example correctly', function() {
+			var example_unencrypted = '000900010002AB43E3D500FF001145327643D4F100';
+			var example_encrypted = '00091D4F3285B60CD150B6F3D141F3C1A7022702F3';
 
-            var encrypted = Application.prototype.decrypt(new Buffer(example_encrypted, 'hex'), '1D4E3287');
-            encrypted.toString('hex').toUpperCase().should.equal(example_unencrypted);
-        })
-    })
+			var encrypted = Application.prototype.decrypt(new Buffer(example_encrypted, 'hex'), '1D4E3287');
+			encrypted.toString('hex').toUpperCase().should.equal(example_unencrypted);
+		})
+	})
 
-    describe('checkAndRemoveCRC', function() {
+	describe('checkAndRemoveCRC', function() {
 
-        it('should throw an error if the crc is invalid', function() {
-            try {
-                Application.prototype.checkAndRemoveCRC(new Buffer('00091D4F3285B60CD150B6F3D141F3C1A7022702F3', 'hex'));
-            } catch (err) {
-                return;
-            }
-            throw new Error('Should have thrown an error');
-        })
+		it('should throw an error if the crc is invalid', function() {
+			try {
+				Application.prototype.checkAndRemoveCRC(new Buffer('00091D4F3285B60CD150B6F3D141F3C1A7022702F3', 'hex'));
+			} catch (err) {
+				return;
+			}
+			throw new Error('Should have thrown an error');
+		})
 
-        it('should check and remove the CRC from the buffer if the CRC is valid', function() {
+		it('should check and remove the CRC from the buffer if the CRC is valid', function() {
 
-            var newBuffer = Application.prototype.checkAndRemoveCRC(new Buffer('00091D4F3285B60CD150B6F3D141F3C1A702276dE4', 'hex'));
-            newBuffer.toString('hex').toUpperCase().should.equal('00091D4F3285B60CD150B6F3D141F3C1A70227');
-        })
-    })
+			var newBuffer = Application.prototype.checkAndRemoveCRC(new Buffer('00091D4F3285B60CD150B6F3D141F3C1A702276dE4', 'hex'));
+			newBuffer.toString('hex').toUpperCase().should.equal('00091D4F3285B60CD150B6F3D141F3C1A70227');
+		})
+	})
 
 })
--- test/xor_spec.js
+++ test/xor_spec.js
@@ -3,55 +3,55 @@ var xor = require('../lib/xor')
 
 describe('xor', function() {
 
-    describe('encrypt', function() {
+	describe('encrypt', function() {
 
-        it('should encrypt the anh example correctly', function() {
-            var example_unencrypted = '00010002AB43E3D500FF001145327643D4F100';
-            var example_encrypted = '1D4F3285B60CD150B6F3D141F3C1A7022702F3';
+		it('should encrypt the anh example correctly', function() {
+			var example_unencrypted = '00010002AB43E3D500FF001145327643D4F100';
+			var example_encrypted = '1D4F3285B60CD150B6F3D141F3C1A7022702F3';
 
-            var encrypted = xor.encrypt({
-                buffer: new Buffer(example_unencrypted, 'hex'),
-                crcSeed: '1D4E3287'
-            });
-            encrypted.toString('hex').toUpperCase().should.equal(example_encrypted);
-        })
+			var encrypted = xor.encrypt({
+				buffer: new Buffer(example_unencrypted, 'hex'),
+				crcSeed: '1D4E3287'
+			});
+			encrypted.toString('hex').toUpperCase().should.equal(example_encrypted);
+		})
 
-        it('should encrypt this correctly', function() {
-            var unencrypted = '789c636138366dd32a1b0606060520160562be6b93ef5d7abfbf6fe19a4bef7c42f719c4ef53e6ddb23ae267526665fa0b81bccb6db3adbca48a16feea9ff67ffed2a4284d410628606528c9c8cf2f000039301f673031';
-            var expected = 'dd96edebe5a08038cfbb863ec9bea628ccdc18435f334539e08c2ad87ac7c5a43830dc60d7633abd6559d8da373fbd203cbe01eb510dac57f587baa91f184cd6e1cae8feac8beed6cceec61f0421e91f0418d900633435';
+		it('should encrypt this correctly', function() {
+			var unencrypted = '789c636138366dd32a1b0606060520160562be6b93ef5d7abfbf6fe19a4bef7c42f719c4ef53e6ddb23ae267526665fa0b81bccb6db3adbca48a16feea9ff67ffed2a4284d410628606528c9c8cf2f000039301f673031';
+			var expected = 'dd96edebe5a08038cfbb863ec9bea628ccdc18435f334539e08c2ad87ac7c5a43830dc60d7633abd6559d8da373fbd203cbe01eb510dac57f587baa91f184cd6e1cae8feac8beed6cceec61f0421e91f0418d900633435';
 
-            var encrypted = xor.encrypt({
-                buffer: new Buffer(unencrypted, 'hex'),
-                crcSeed: 'a50a8e8a'
-            });
-            encrypted.toString('hex').should.equal(expected);
-        })
-    })
+			var encrypted = xor.encrypt({
+				buffer: new Buffer(unencrypted, 'hex'),
+				crcSeed: 'a50a8e8a'
+			});
+			encrypted.toString('hex').should.equal(expected);
+		})
+	})
 
-    describe('decrypt', function() {
-        
-        it('should decrypt the anh example correctly', function() {
-            var example_unencrypted = '00010002AB43E3D500FF001145327643D4F100';
-            var example_encrypted = '1D4F3285B60CD150B6F3D141F3C1A7022702F3';
+	describe('decrypt', function() {
 
-            var decrypted = xor.decrypt({
-                buffer: new Buffer(example_encrypted, 'hex'),
-                crcSeed: '1D4E3287'
-            });
-            decrypted.toString('hex').toUpperCase().should.equal(example_unencrypted);
-        })
+		it('should decrypt the anh example correctly', function() {
+			var example_unencrypted = '00010002AB43E3D500FF001145327643D4F100';
+			var example_encrypted = '1D4F3285B60CD150B6F3D141F3C1A7022702F3';
 
-        it('should decrypt this correctly', function() {
-            var encrypted = 'dd96edebe5a08038cfbb863ec9bea628ccdc18435f334539e08c2ad87ac7c5a43830dc60d7633abd6559d8da373fbd203cbe01eb510dac57f587baa91f184cd6e1cae8feac8beed6cceec61f0421e91f0418d900633435';
-            var expected = '789c636138366dd32a1b0606060520160562be6b93ef5d7abfbf6fe19a4bef7c42f719c4ef53e6ddb23ae267526665fa0b81bccb6db3adbca48a16feea9ff67ffed2a4284d410628606528c9c8cf2f000039301f673031';
+			var decrypted = xor.decrypt({
+				buffer: new Buffer(example_encrypted, 'hex'),
+				crcSeed: '1D4E3287'
+			});
+			decrypted.toString('hex').toUpperCase().should.equal(example_unencrypted);
+		})
 
-            var decrypted = xor.decrypt({
-                buffer: new Buffer(encrypted, 'hex'),
-                crcSeed: 'a50a8e8a'
-            });
-            decrypted.toString('hex').should.equal(expected);
+		it('should decrypt this correctly', function() {
+			var encrypted = 'dd96edebe5a08038cfbb863ec9bea628ccdc18435f334539e08c2ad87ac7c5a43830dc60d7633abd6559d8da373fbd203cbe01eb510dac57f587baa91f184cd6e1cae8feac8beed6cceec61f0421e91f0418d900633435';
+			var expected = '789c636138366dd32a1b0606060520160562be6b93ef5d7abfbf6fe19a4bef7c42f719c4ef53e6ddb23ae267526665fa0b81bccb6db3adbca48a16feea9ff67ffed2a4284d410628606528c9c8cf2f000039301f673031';
 
-        })
-    })
+			var decrypted = xor.decrypt({
+				buffer: new Buffer(encrypted, 'hex'),
+				crcSeed: 'a50a8e8a'
+			});
+			decrypted.toString('hex').should.equal(expected);
+
+		})
+	})
 
 })
